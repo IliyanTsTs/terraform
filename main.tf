b64ab7b4293c4631d55f5d49d5d43667e3c85a1d
IliyanTsTs <iliyan.ts.ts@gmail.com>
2022-01-06 20:54:26 +0200

    added nginx

--- main.tf
+++ main.tf
@@ -22,3 +22,18 @@ resource "docker_container" "jenkins" {
     external = 8080
   }
 }
+
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.latest
+  name  = "tutorial"
+  ports {
+    internal = 80
+    external = 8000
+  }
+}
